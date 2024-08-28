2162485cb51d7fc9aba04e76b34245ddd55646a6
Matthew Cronin <mattslaptop@gmail.com>
2024-08-28 15:02:41 -0400

    update

--- Controllers/ESCController.cs
+++ Controllers/ESCController.cs
@@ -256,7 +256,7 @@ namespace ESC_Assessment.Controllers
             SqlConnection con = new SqlConnection("Server=tcp:mcroninpersonal.database.windows.net,1433;Initial Catalog=ESC;Persist Security Info=False;User ID=mcroninSQL;Password=TestingDB1!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             SqlCommand cmd = new SqlCommand();
             cmd.Connection = con;
-            cmd.CommandText = "Select first_name + ' ' + last_name as Display, dependent_id as ID from jobs FOR JSON PATH";
+            cmd.CommandText = "Select first_name + ' ' + last_name as Display, dependent_id as ID from dependents FOR JSON PATH";
             con.Open();
             string ret = "";
             using (SqlDataReader rdr = cmd.ExecuteReader())
