1fb15c86cbc843b9d5ca6cd58285ce608c419c50
Matthew Cronin <mattslaptop@gmail.com>
2024-08-28 17:37:09 -0400

    Change data type

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -7,7 +7,7 @@
         public string LastName { get; set; } = "";
         public string Email { get; set; } = "";
         public string PhoneNumber { get; set; } = "";
-        public DateTime HireDate { get; set; }
+        public string HireDate { get; set; }
         public int JobID { get; set; }
         public decimal Salary { get; set; }
         public int DepartmentID { get; set; }
